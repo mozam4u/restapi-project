2182a4c8879f7e84e07e8626f8257d86ff201ff6
mozammil hassan <78438006+mozam4u@users.noreply.github.com>
2022-05-15 13:34:41 +0530

    Add files via upload

---
+++ models/model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    name: {
+          required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String,
+        unique:true,        
+
+    }
+})
+
+module.exports = mongoose.model('Data', dataSchema)
---
+++ routes/routes.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const res = require('express/lib/response');
+//const { collection } = require('../models/model');
+const Model = require('../models/model');
+const router = express.Router();
+const bodyParser=require ('body-parser');
+// Post Method working(success)
+
+router.use(bodyParser.json());
+
+router.post('/postNew', (req, res) => {
+        console.log("hi", req.body);
+        Model.create({
+            name: req.body.name,
+            email: req.body.email,
+        }, function(err, result) {
+
+            if (err) return res.send({ "status": false, "message": err })
+            return res.send({ "status": true, "message": "The User's account successfully created" ,"Result":result});
+        })
+    })
+    //     //-- -- -- -- -- -- -- -- -- -- -- - get type working(success)-- -- -- -- -- -- -- -- -- -- -
+
+router.get('/:postId', async(request, response) => {
+        Model.find({
+            "_id": request.params.postId
+        }, (error, result) => {
+            console.log("test", result);
+            if (result) {
+
+                return response.send({ "status": true, "message": "record found", "Result": result });
+            }
+            return response.status(500).send(error);
+            //response.send(result);
+        })
+    })
+
+    //     //-- -- -- -- -- -- -- -- -- -- -- - get type working(success)-- -- -- -- -- -- -- -- -- -- -
+    router.get('/:postId', async(request, response) => {
+        Model.find({
+            "_id": request.params.postId
+        }, (error, result) => {
+            console.log("test", result);
+            if (result) {
+
+                return response.send({ "status": true, "message": "record found", "Result": result });
+            }
+            return response.status(500).send(error);
+            //response.send(result);
+        })
+    })
+
+   
+    //     //--------------------------------delete method working(success)----------------------------
+router.delete('/:postId', async(request, response) => {
+    Model.findByIdAndDelete({
+        "_id": request.params.postId
+    }, (error, result) => {
+        console.log("test", result);
+        if (result) {
+
+            return response.send({ "status": true, "message": "record deleted successfully", "Result": result });
+        }
+        return response.status(500).send(error);
+
+    })
+})
+
+//------------------update  working(success)------
+router.patch('/:postId', async(req, res) => {
+
+    Model.updateOne({
+            _id: req.params.postId
+        }, {
+            $set: {
+                name: req.body.name,
+                email: req.body.email
+
+            },
+        },
+        function(err, callback) {
+
+            if (err) return res.send({ "status": false, "message": err })
+            return res.send({ "status": true, "message": "The User's account successfully updated", "Result": callback });
+
+        }
+    )
+});
+
+//    -----------------------------put working(success)-------------------
+router.put('/:postId', async(req, res) => {
+
+    Model.findOneAndUpdate({
+            _id: req.params.postId
+        }, {
+            $set: {
+                name: req.body.name,
+                email: req.body.email
+
+            },
+        },
+        function(err, callback) {
+
+            if (err) return res.send({ "status": false, "message": err })
+
+            return res.send({ "status": true, "message": "The User's account successfully updated", "Result": callback });
+        }
+    )
+})
+module.exports = router;
+
+
